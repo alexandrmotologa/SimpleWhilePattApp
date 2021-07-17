2e1f962be0212a7f93ad3c6e69f4bfbf0c6ebe68
alexandrmotologa <al.motologa@gmail.com>
2021-07-17 14:14:59 +0300

    hw

--- SimpleWhilePattApp/src/SimpleWhilePattApp.java
+++ SimpleWhilePattApp/src/SimpleWhilePattApp.java
@@ -4,7 +4,6 @@ public class SimpleWhilePattApp {
 		int rx = 6, ry =2;
 		int y = 1;
 		
-		
 		while( y <= 10) {
 		
 			int x = 1;
@@ -13,24 +12,18 @@ public class SimpleWhilePattApp {
 				
 				if(x == 1 || x == 10) {
 					System.out.print("* ");
-					
 				} else if(x >= 2 && y == 1 ){
 					System.out.print("* ");
-					
 				} else if(x >= 2 && y == 10 ){
 					System.out.print("* ");
-					
 				} else {
 					System.out.print("  ");
-					
 				} 
 				if(x == rx && y == ry ) {
 					if(ry !=10 && rx !=10 && ry !=1 && rx !=1) {
 						System.out.print("\b+");
-						
 					} else {
 						System.out.print("");
-						
 					}
 				}
 				x++;
